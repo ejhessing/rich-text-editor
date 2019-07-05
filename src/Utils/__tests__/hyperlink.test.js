6c290595395598f67d3ca1eb48a3b98de6bb2ba2
Erwin Hessing <ejhessing@gmail.com>
2019-07-05 20:53:47 +1200

    Update hyperlink tests

--- src/Utils/__tests__/hyperlink.test.js
+++ src/Utils/__tests__/hyperlink.test.js
@@ -1,88 +1,47 @@
 import Raw from "draft-js-raw-content-state";
-import { EditorState } from "draft-js";
+import { EditorState, convertFromHTML, ContentState } from "draft-js";
 import { insertLink, removeLink, getLinkDetails } from "../hyperlink";
 
 const linkValue = "https://google.com";
 const linkTitle = "Google";
 
 describe("Hyperlink - insertLink", () => {
-  describe("Non-collapsed section", () => {
-    const editorState = new Raw()
-      .addBlock("abc")
-      .setKey("edr45")
-      .anchorKey(0)
-      .focusKey(6)
-      .toEditorState();
+  const contentBlocks = convertFromHTML("<div>test</div>");
+  const contentState = ContentState.createFromBlockArray(contentBlocks);
+  const editorState = EditorState.createWithContent(contentState);
 
-    const newEditorState = insertLink({ editorState, linkValue, linkTitle });
+  const newEditorState = insertLink({ editorState, linkValue, linkTitle });
 
-    it("should add a hyperlink and title", () => {
-      const contentState = newEditorState.getCurrentContent();
-      const { link, title } = contentState.getEntity("1").getData();
-      expect(link).toBe(linkValue);
-      expect(title).toBe(linkTitle);
-    });
-
-    it("should remove the hyperlink", () => {
-      const addSelection = newEditorState.getSelection().merge({
-        anchorOffset: 0,
-        focusOffset: 6
-      });
-
-      const esWithNewSelection = EditorState.acceptSelection(
-        newEditorState,
-        addSelection
-      );
-
-      const editorStateWithoutLink = removeLink({
-        editorState: esWithNewSelection
-      });
-
-      const { link, title } = getLinkDetails({
-        editorState: editorStateWithoutLink
-      });
-
-      expect(link).toBeUndefined();
-      expect(title).toBeUndefined();
-    });
+  it("should add a hyperlink and title", () => {
+    const contentState = newEditorState.getCurrentContent();
+    const { link, title } = contentState.getEntity("1").getData();
+    expect(link).toBe(linkValue);
+    expect(title).toBe(linkTitle);
   });
 
-  describe("Collapsed Section", () => {
-    const editorState = new Raw()
-      .addBlock("")
-      .collapse(3)
-      .toEditorState();
-
-    const newEditorState = insertLink({ editorState, linkValue, linkTitle });
-
-    it("should add a hyperlink and title", () => {
-      const contentState = newEditorState.getCurrentContent();
-      const { link, title } = contentState.getEntity("1").getData();
-      expect(link).toBe(linkValue);
-      expect(title).toBe(linkTitle);
+  it("should remove the hyperlink", () => {
+    const addSelection = newEditorState.getSelection().merge({
+      anchorOffset: 0,
+      focusOffset: 6
     });
 
-    it("should remove the hyperlink", () => {
-      const addSelection = newEditorState.getSelection().merge({
-        anchorOffset: 0,
-        focusOffset: 6
-      });
+    const esWithNewSelection = EditorState.acceptSelection(
+      newEditorState,
+      addSelection
+    );
 
-      const esWithNewSelection = EditorState.acceptSelection(
-        newEditorState,
-        addSelection
-      );
-
-      const editorStateWithoutLink = removeLink({
-        editorState: esWithNewSelection
-      });
-
-      const { link, title } = getLinkDetails({
-        editorState: editorStateWithoutLink
-      });
-
-      expect(link).toBeUndefined();
-      expect(title).toBeUndefined();
+    const editorStateWithoutLink = removeLink({
+      editorState: esWithNewSelection
     });
+
+    const { link, title } = getLinkDetails({
+      editorState: editorStateWithoutLink
+    });
+
+    expect(link).toBeUndefined();
+    expect(title).toBeUndefined();
   });
+
+  it("should get the correct details if there is a link", () => {});
+  it("should not return anything if there is no link present", () => {});
 });
